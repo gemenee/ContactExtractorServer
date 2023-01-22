144761ba1268a045f9a7e4b6f0f69f27a7de631b
Nikita Chertkov <69633289+gemenee@users.noreply.github.com>
2023-01-22 22:33:39 +0300

    Update Extract.py

--- app/Extract.py
+++ app/Extract.py
@@ -1,21 +1,21 @@
 from flask import jsonify, request
 import pickle
-import sys
 from yargy.interpretation import fact
 
 from app import app
 from app.Extractor import extractor
 from app.Extractor.person import Person
 
-
 @app.route('/extract', methods=['GET', 'POST'])
 def extract():
+
     if request.method == 'POST':
-        text = request.data.decode(sys.stdout.encoding)
+        text = request.data.decode('utf-8')
         facts = extractor.extract_facts(text)
-        result = []
+        result = [{}]
         for f in facts:
-            result.append(f.as_json)
-        return result
+            if f is not None:
+                result.append(f.as_json)
+        return jsonify(result)
     else:
         return [{}]
